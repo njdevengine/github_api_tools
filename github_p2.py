70184b0fc0099c0d74176a91c69b4eb4f1e7970c
njdevengine <24538036+njdevengine@users.noreply.github.com>
2020-08-17 01:43:56 -0400

    Update github_p2.py

--- github_p2.py
+++ github_p2.py
@@ -3,3 +3,14 @@
 import pandas as pd
 from github import Github
 g = Github("YOUR API KEY")
+
+####https://docs.github.com/en/github/searching-for-information-on-github/searching-for-repositories
+#    search = g.search_repositories("language:python followers:>=100 stars:>=500 created:>=2020-08-01", "stars", "desc").get_page(i)
+searches = []
+for i in range(0,11):
+    search = g.search_repositories("followers:>=100 stars:>=500 created:>=2020-08-01", "stars", "desc").get_page(i)
+    searches.append(search)
+    
+#remove empty searches
+while([] in searches) : 
+    searches.remove([]) 
