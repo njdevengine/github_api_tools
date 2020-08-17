08d71d89627aaa965fc6c6ddcef2a88e5529ac6b
njdevengine <24538036+njdevengine@users.noreply.github.com>
2020-08-17 01:47:03 -0400

    Update github_p2.py

--- github_p2.py
+++ github_p2.py
@@ -37,3 +37,45 @@ for i in range(len(searches)):
         descs.append(searches[i][n].raw_data["description"])
         
 df = pd.DataFrame({"Repo Name":repo_names,"Description":descs,"Owner":repo_owners,"Stars":star_count,"Subscribers":subs,"Language":langs,"Link":links})
+
+#alternatively using github trends API https://github.com/huchenme/github-trending-api
+#sample trends url
+#https://ghapi.huchen.dev/repositories?language=javascript&since=weekly&spoken_language_code=english
+import requests
+import json
+
+weekly_url = "https://ghapi.huchen.dev/repositories?&since=weekly&spoken_language_code=english"
+today_url = "https://ghapi.huchen.dev/repositories?&since=today&spoken_language_code=english"
+
+def get_trends(link,filename):
+    with requests.Session() as s:
+        url = link
+        r = s.get(url)
+    data = r.json()
+    
+    repo_names = []
+    repo_owners = []
+    star_count = []
+    links = []
+    langs = []
+    descs = []
+
+    for i in range(len(data)):
+        repo_names.append(data[i]["name"])
+        descs.append(data[i]["description"])
+        repo_owners.append(data[i]["author"])
+        star_count.append(data[i]["currentPeriodStars"])
+        links.append(data[i]["url"]+"/"+data[i]["name"])
+        try:
+            langs.append(data[i]["language"])
+        except:
+            langs.append("")
+
+    trends = pd.DataFrame({"Repo Name":repo_names,"Description":descs,"Owner":repo_owners,"Stars":star_count,"Language":langs,"Link":links}).sort_values(by="Stars",ascending=False)
+    trends.to_csv(filename+".csv",index=False)
+    
+get_trends(weekly_url,"weekly_trends")
+get_trends(today_url,"today_trends")
+
+week = pd.read_csv("weekly_trends.csv")
+day = pd.read_csv("today_trends.csv")
